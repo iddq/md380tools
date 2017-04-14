dd8e697f8d2f4ef25365231de1e51ce094fc68b5
iddq <iddq>
2017-04-14 09:27:45 +0200

    led process replacement

--- applet/src/led.c
+++ applet/src/led.c
@@ -1,8 +1,8 @@
 #include "led.h"
 
-extern void sleep(__IO uint32_t ms);
+//extern void md380_sleep(__IO uint32_t ms);
 
-void led_process_replacement() {
+void __attribute__((optimize("O0"))) led_process_replacement() {
     static int led_status = 1;
     
     while (1) {
@@ -13,7 +13,21 @@ void led_process_replacement() {
             LED_RED_ON
             LED_GREEN_OFF
         }
-        sleep(100);
+
         led_status ^= 1;
+
+        //md380_sleep(333); // Why the audio doesn't work without sleep?
+
+//ROM:080A1086 64 20                       MOVS            R0, #0x64
+//ROM:080A1088 92 F7 14 FF                 BL              sub_8033EB4
+//ROM:080A108C E0 E7                       B               loc_80A1050
+
+        ((void (*)(uint32_t))0x08033EB4+1)(333); // calling the sleep in different way. Why does the compiler place blx into the code?.
+
+//ROM:080A1084 1A 60                       STR             R2, [R3]
+//ROM:080A1086 04 4B                       LDR             R3, =sub_8033EB4
+//ROM:080A1088 64 20                       MOVS            R0, #0x64
+//ROM:080A108A 98 47                       BLX             R3 ; sub_8033EB4
+//ROM:080A108C E0 E7                       B               loc_80A1050
     }
 }
